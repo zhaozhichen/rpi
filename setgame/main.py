bf1ea2820885949e6dd5bb9ec5f3bc481444d9ac
Zhichen Zhao <zhaozhichen@gmail.com>
2017-02-04 21:26:54 -0500

    update

--- setgame/main.py
+++ setgame/main.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 from setgameLib import *
-trainSet = ['train/1.jpg']
+trainSet = ['train/'+str(i)+'.jpg' for i in range(9)]
 
 for i, card in enumerate(train(trainSet)):
     savefile = 'images/res'+str(i)+'.jpg'
